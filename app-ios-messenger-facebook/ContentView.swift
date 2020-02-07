34c00594974eaf1f31469e1e226e8e0842617bb1
Aphirat Nimanussonkul <tata@odds.team>
2020-02-07 19:56:30 +0700

    Initial Commit

---
+++ app-ios-messenger-facebook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  app-ios-messenger-facebook
+//
+//  Created by ODDS on 7/2/2563 BE.
+//  Copyright © 2563 ODDS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

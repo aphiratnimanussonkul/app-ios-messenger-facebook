ccaa6d81333acb99bd0685da824a99ae111f7b3e
Aphirat Nimanussonkul <tata@odds.team>
2020-02-07 20:33:28 +0700

    created class firend and set background color blue, then show friendclass for 3 item in main FriendController

--- app-ios-messenger-facebook/ContentView.swift
+++ app-ios-messenger-facebook/ContentView.swift
@@ -2,9 +2,38 @@ import SwiftUI
 
 class FriendsController: UICollectionViewController {
     
+    private let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         collectionView?.backgroundColor = UIColor.red
+        
+        collectionView?.register(FriendCell.self, forCellWithReuseIdentifier: cellId)
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    }
+    
+}
+
+class FriendCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implement")
+    }
+    
+    func setupView() {
+        backgroundColor = UIColor.blue
+    }
+    
 }

e4a1a21cccdd0483af375eb4ceb10d0fba689e87
Aphirat Nimanussonkul <tata@odds.team>
2020-02-26 14:11:34 +0700

    change rootviewcontroller  from friendsController to TabBarControlller

---
+++ app-ios-messenger-facebook/Controllers/TabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarController.swift
+//  app-ios-messenger-facebook
+//
+//  Created by ODDS on 26/2/2563 BE.
+//  Copyright © 2563 ODDS. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: Tab {
+
+}
--- app-ios-messenger-facebook/SceneDelegate.swift
+++ app-ios-messenger-facebook/SceneDelegate.swift
@@ -20,13 +20,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let layout = UICollectionViewFlowLayout()
-        let friendsController = FriendsController(collectionViewLayout: layout)
+        
+//        let layout = UICollectionViewFlowLayout()
+//        let friendsController = FriendsController(collectionViewLayout: layout)
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UINavigationController(rootViewController: friendsController)
+            window.rootViewController = TabBarController()
             self.window = window
             window.makeKeyAndVisible()
         }

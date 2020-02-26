f7944f7ae3877c185f153ce7dabb909acce26be5
Aphirat Nimanussonkul <tata@odds.team>
2020-02-26 10:34:21 +0700

    create func createMessageWithText and refactor code

--- app-ios-messenger-facebook/Controllers/FriendsControllerHelper.swift
+++ app-ios-messenger-facebook/Controllers/FriendsControllerHelper.swift
@@ -14,37 +14,26 @@ extension FriendsController {
             aphirat.name = "Aphirat Nimanussonkul"
             aphirat.profileImageName = "aphirat_profile"
             
-            let messageAphirat = NSEntityDescription.insertNewObject(forEntityName: "Message", into: context) as! Message
-            messageAphirat.text = "Hello IOS"
-            messageAphirat.date = NSDate() as Date
-            messageAphirat.friend = aphirat
+            createMessageWithText(text: "Hello", minuteAgo: 1, friend: aphirat, context: context)
             
             let nattiya = NSEntityDescription.insertNewObject(forEntityName: "Friend", into: context) as! Friend
             nattiya.name = "Nattiya Nimanussonkul"
             nattiya.profileImageName = "nattiya_profile"
             
-            let messageNattiya = NSEntityDescription.insertNewObject(forEntityName: "Message", into: context) as! Message
-            messageNattiya.text = "hey how r u today"
-            messageNattiya.date = NSDate() as Date
-            messageNattiya.friend = nattiya
+            createMessageWithText(text: "hey how r u today", minuteAgo: 1, friend: nattiya, context: context)
             
             let chanantapol = NSEntityDescription.insertNewObject(forEntityName: "Friend", into: context) as! Friend
             chanantapol.name = "Chanantapol Nimanussonkul"
             chanantapol.profileImageName = "chanantapol_profile"
             
-            let messageChanantapol = NSEntityDescription.insertNewObject(forEntityName: "Message", into: context) as! Message
-            messageChanantapol.text = "Im on my way"
-            messageChanantapol.date = NSDate() as Date
-            messageChanantapol.friend = chanantapol
+            createMessageWithText(text: "I'm on my way", minuteAgo: 2, friend: chanantapol, context: context)
             
             let nattamon = NSEntityDescription.insertNewObject(forEntityName: "Friend", into: context) as! Friend
             nattamon.name = "Nattamon KlongDee"
             nattamon.profileImageName = "nattamon_profile"
             
-            let messageNattamon = NSEntityDescription.insertNewObject(forEntityName: "Message", into: context) as! Message
-            messageNattamon.text = "good morning son"
-            messageNattamon.date = NSDate() as Date
-            messageNattamon.friend = nattamon
+            createMessageWithText(text: "Good morning son", minuteAgo: 2, friend: nattamon, context: context)
+            createMessageWithText(text: "How r u?", minuteAgo: 3, friend: nattamon, context: context)
             
             do {
                 try context.save()
@@ -53,6 +42,19 @@ extension FriendsController {
             }
         }
         loadData()
+    }
+    
+    private func createMessageWithText(text: String, minuteAgo: Double, friend: Friend, context: NSManagedObjectContext) {
+        let message = NSEntityDescription.insertNewObject(forEntityName: "Message", into: context) as! Message
+        message.text = text
+        message.date = NSDate().addingTimeInterval(-minuteAgo * 60) as Date
+        message.friend = friend
+        
+        do {
+            try context.save()
+        } catch {
+            print(error)
+        }
     }
     
     func loadData(){
